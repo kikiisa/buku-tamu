2ece1673cddb5f1933cd45e2cb31b03864ecc8ad
kikiisa <kikiisa89@gmail.com>
2023-11-30 20:49:56 +0800

    new db

--- buku_tamu.sql
+++ buku_tamu.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 26, 2023 at 02:21 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.0.28
+-- Generation Time: Nov 30, 2023 at 01:49 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `dbbukutamu`
+-- Database: `buku_tamu`
 --
 
 -- --------------------------------------------------------
@@ -33,25 +33,17 @@ CREATE TABLE `ttamu` (
   `nama` varchar(100) NOT NULL,
   `alamat` varchar(100) NOT NULL,
   `tujuan` varchar(100) NOT NULL,
-  `nope` varchar(20) NOT NULL
+  `nope` varchar(20) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `asal` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `ttamu`
 --
 
-INSERT INTO `ttamu` (`id`, `tanggal`, `nama`, `alamat`, `tujuan`, `nope`) VALUES
-(1, '2023-11-15', 'Rizal ahmad', 'kabila', 'bimbingan', '089765435689'),
-(2, '2023-11-20', 'Indah', 'kota', 'konsultasi', '0819864579'),
-(3, '2023-11-23', 'indah', 'kabila', 'bimbingan', '7653859699'),
-(4, '2023-11-22', 'yati', 'korea', 'bertamu', '988547476'),
-(5, '2023-10-08', 'devi', 'suwawa', 'lamar kerja', '08527382837'),
-(6, '2023-11-25', 'sf', 'hjm', 'myhvgmj', 'vgbj,hb'),
-(7, '2023-11-25', ',kb,h', 'kjb,jhb', 'kb,k', 'uh,'),
-(8, '2023-11-25', 'ads', 'uyf', 'tfv', 'utfvm'),
-(9, '2023-11-25', 'wf', 'wef', 'wed', 'wfsd'),
-(10, '2023-11-25', 'yvgmhf', 'nhcd', 'htdn', 'fgxb'),
-(11, '2023-11-25', 'bhj,', 'hv n', 'vh m', 'v hm');
+INSERT INTO `ttamu` (`id`, `tanggal`, `nama`, `alamat`, `tujuan`, `nope`, `image`, `asal`) VALUES
+(15, '2023-11-30', 'Mohamad Rizky Isa', 'Suwawa', 'Bertemu Kabid', '0885932961131', 'image/65686c1f2cb3b.png', 'Kominfo Dan Dishub');
 
 -- --------------------------------------------------------
 
@@ -72,7 +64,7 @@ CREATE TABLE `tuser` (
 --
 
 INSERT INTO `tuser` (`id_user`, `username`, `password`, `nama_pengguna`, `status`) VALUES
-(1, 'admin', '0192023a7bbd73250516f069df18b500', 'Administrator', 'Aktif');
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Administrator', 'Aktif');
 
 --
 -- Indexes for dumped tables
@@ -98,7 +90,7 @@ ALTER TABLE `tuser`
 -- AUTO_INCREMENT for table `ttamu`
 --
 ALTER TABLE `ttamu`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `tuser`
